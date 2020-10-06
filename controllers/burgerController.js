ad1ccc741bae2b1bee1e2732eeba1a010381c42d
Katelynn <ktlnn.nguyen@gmail.com>
2020-10-06 15:29:24 -0400

    creating handlebars and burgerController

--- controllers/burgerController.js
+++ controllers/burgerController.js
@@ -4,5 +4,78 @@ const router = express.Router();
 
 const burger = require("../models/burger");
 
+//reads the model and renders it all to the page
+router.get("/", function (req, res) {
+    burger.read()
+    .then(data => {
+        var hbsObject = {
+            burgers: data
+        }
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+});
 
+
+//allows for the creation of a new object(burger)
+router.post("/api/cats", function (req, res) {
+    //gets the columns
+    var cols = Object.entries(req.body).map(e => e[0]);
+    //gets the values
+    var vals = Object.entries(req.body).map(e => e[1]); 
+
+    burger.create(cols, vals)
+    .then(results => {
+        console.log(results);
+        res.json({id: results.insertId});
+    })
+    .catch(err => {
+        console.log(err);
+    });
+});
+
+//allows for users to update object(burger)
+router.put("/api/cats/:id", function (req,res) {
+    var condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
+
+    burger.update({
+        devoured: req.body.devoured
+    }, condition)
+    .then(result => {
+        if (result.channgedRows == 0){
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    });
+});
+
+
+//allows for user to delete object(burger)
+router.delete("/api/cats/:id", function (req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.delete(condition)
+    .then(result => {
+        if (result.affectedRows ==0) {
+            return res.status(404).end();
+        } else {
+            return res.status(200).end();
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    });
+});
+
+//Export routes for server.js to use
+module.exports = router;
 
